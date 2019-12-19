40087d5132981b02356a03207a36968e8e80745b
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:09:26 -0500

    se creó el archivo Ejercicio45.py

---
+++ Castillo/Ejercicio45.py
@@ -0,0 +1,8 @@
+import libreria
+import os
+
+#Calcular el area de un paralelepipedo
+area_paralelepipedo=libreria.pedir_area_paralelepipedo(int(os.sys.argv[1]),int(os.sys.argv[2]),int(os.sys.argv[3]))
+print ("El area de un paralelepipedo es: " , area_paralelepipedo)
+
+
