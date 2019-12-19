6ef77e0767e53d0eac66b97f55cfc6537ebf3481
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:05:26 -0500

    se creó el archivo Ejercicio32.py

---
+++ Castillo/Ejercicio32.py
@@ -0,0 +1,8 @@
+import libreria
+import os
+
+#Calcular el trabajo de un cuerpo
+trabajo_de_un_cuerpo = libreria.trabajo_de_cuerpo(int(os.sys.argv[1]), int(os.sys.argv[2]))
+print("El trabajo de un cuerpo es: " , trabajo_de_un_cuerpo)
+
+
