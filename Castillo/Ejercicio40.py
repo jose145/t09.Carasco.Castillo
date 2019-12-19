45ebfc210c488c6cf0a13db65d4fa15e1420b651
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:08:02 -0500

    se creó el archivo Ejercicio40.py

---
+++ Castillo/Ejercicio40.py
@@ -0,0 +1,8 @@
+import libreria
+import os
+
+#Calcular el area del paralelogramo
+area = libreria.area_paralelogramo(int(os.sys.argv[1]),int(os.sys.argv[2]))
+print ("El area del paralelogramo es: " , int(area))
+
+
