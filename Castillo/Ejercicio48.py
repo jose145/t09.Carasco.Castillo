615adf2e83631726c99158a35ea4fce39b16782a
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:10:13 -0500

    se creó el archivo Ejercicio48.py

---
+++ Castillo/Ejercicio48.py
@@ -0,0 +1,7 @@
+import libreria
+import os
+
+#Calcular el area de un cilindro
+area_cilindro=libreria.pedir_area_cilindro(int(os.sys.argv[1]),int(os.sys.argv[2]))
+print ("El area de un cilindro es: " , area_cilindro)
+
