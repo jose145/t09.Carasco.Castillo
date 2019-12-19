86f3fb60f53c85b83ce5745dbe6dd813e79b5317
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:05:06 -0500

    se creó el archivo Ejercicio31.py

---
+++ Castillo/Ejercicio31.py
@@ -0,0 +1,7 @@
+import libreria
+import os
+
+#Calcular el area del triangulo
+area = libreria.area_del_triangulo(int(os.sys.argv[1]), int(os.sys.argv[2]))
+print ("El area del triangulo es: " , int(area))
+
