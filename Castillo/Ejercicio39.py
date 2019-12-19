a8d869b45c0eca4676dae794b7fc6e44ea2dc969
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:07:45 -0500

    se creó el archivo Ejercicio39.py

---
+++ Castillo/Ejercicio39.py
@@ -0,0 +1,6 @@
+import libreria
+import os
+
+#Calcular la energia cinetica
+energia_cinetica = libreria.energia_cinetica(int(os.sys.argv[1]),int(os.sys.argv[2]))
+print ("La energia cinetica es: " , int(energia_cinetica))
