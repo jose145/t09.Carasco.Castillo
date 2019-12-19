3275ffe96cba92d6684994c9d3477d26558de4c2
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:03:59 -0500

    se creó el archivo Ejercicio30.py

---
+++ Castillo/Ejercicio30.py
@@ -0,0 +1,8 @@
+import libreria
+import os
+
+#Calcular la densidad de un cuerpo
+densidad = libreria.densidad(int(os.sys.argv[1]),int(os.sys.argv[2]))
+print ("La densidad de un cuerpo es: " , int(densidad))
+
+
