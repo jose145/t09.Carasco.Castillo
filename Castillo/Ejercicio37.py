eb3d2aebab2bf9ce262e91a4bae64ba10d827a1b
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:07:11 -0500

    se creó el archivo Ejercicio37.py

---
+++ Castillo/Ejercicio37.py
@@ -0,0 +1,9 @@
+import libreria
+import os
+
+#Calcular el area del trapecio
+area = libreria.area_trapecio(int(os.sys.argv[1]),int(os.sys.argv[2]),int(os.sys.argv[3]))
+print ("El area del trapecio es: " , int(area))
+
+
+
