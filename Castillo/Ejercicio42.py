174427b66f6e4499288ec28d24191b840387ebc0
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:08:38 -0500

    se creó el archivo Ejercicio42.py

---
+++ Castillo/Ejercicio42.py
@@ -0,0 +1,6 @@
+import libreria
+import os
+
+#Calcular la distancia recorrida por un vehiculo
+distancia=libreria.pedir_distancia(int(os.sys.argv[1]),int(os.sys.argv[2]))
+print ("La distancia recorrida por un vehiculo es: " , distancia)
