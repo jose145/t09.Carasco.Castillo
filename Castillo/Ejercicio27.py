125c8600212339ac179782d51ff0dea7a165fa06
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:02:56 -0500

    se creó el archivo Ejercicio27.py

---
+++ Castillo/Ejercicio27.py
@@ -0,0 +1,8 @@
+import libreria
+import os
+
+#Calcular el area del rectangulo
+area = libreria.area_rectangulo(int(os.sys.argv[1]),int(os.sys.argv[2]))
+print ("El area del rectangulo es: " , int(area))
+
+
