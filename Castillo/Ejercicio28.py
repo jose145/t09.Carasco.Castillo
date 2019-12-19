a42e99a85f35606db41f20dd661a2d1c80234f31
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:03:17 -0500

    se creó el archivo Ejercicio28.py

---
+++ Castillo/Ejercicio28.py
@@ -0,0 +1,7 @@
+import libreria
+import os
+
+#Calcular el area del triangulo equilatero
+area = libreria.area_triangulo_equilatero(int(os.sys.argv[1]))
+print ("El area del triangulo equilatero es: " , int(area))
+
