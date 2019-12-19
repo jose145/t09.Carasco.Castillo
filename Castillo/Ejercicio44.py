27ab252bde1d3086b2fa3ebcbc70e2e07dbab91d
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:09:09 -0500

    se creó el archivo Ejercicio44.py

---
+++ Castillo/Ejercicio44.py
@@ -0,0 +1,8 @@
+import libreria
+import os
+
+#Calcular el volumen de un paralelepipedo
+volumen_paralelepipedo=libreria.pedir_volumen_paralelepipedo(int(os.sys.argv[1]),int(os.sys.argv[2]),int(os.sys.argv[3]))
+print ("El volumen de un paralelepipedo es: " , volumen_paralelepipedo)
+
+
