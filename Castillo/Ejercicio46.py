18f2102ba04e87578011db134d73bf3edae59b60
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:09:41 -0500

    se creó el archivo Ejercicio46.py

---
+++ Castillo/Ejercicio46.py
@@ -0,0 +1,8 @@
+import libreria
+import os
+
+#Calcular el volumen de una piramide
+volumen_piramide=libreria.pedir_volumen_piramide(int(os.sys.argv[1]),int(os.sys.argv[2]))
+print ("El volumen de un piramide es: " , volumen_piramide)
+
+
