82e37994a1f6dcbab2be9a744692ee230142733e
Jose <jcastilloj.unprg.edu.pe>
2019-12-19 16:10:46 -0500

    se creó el archivo Ejercicio50.py

---
+++ Castillo/Ejercicio50.py
@@ -0,0 +1,8 @@
+import libreria
+import os
+
+#Calcular el volumen de un como
+volumen_cono=libreria.pedir_volumen_cono(int(os.sys.argv[1]),int(os.sys.argv[2]))
+print ("El volumen de un cono es: " , volumen_cono)
+
+
